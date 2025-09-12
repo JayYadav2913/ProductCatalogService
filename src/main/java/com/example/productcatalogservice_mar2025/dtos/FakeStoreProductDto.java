ab1d1943e13ae673727a32bbf7f5e5aeaef050f9
Jay Yadav <work7028@gmail.com>
2025-09-12 12:57:25 +0530

    Updated FakeStoreProductDto and ProductService

--- src/main/java/com/example/productcatalogservice_mar2025/dtos/FakeStoreProductDto.java
+++ src/main/java/com/example/productcatalogservice_mar2025/dtos/FakeStoreProductDto.java
@@ -6,4 +6,59 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FakeStoreProductDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String category;
+    private Double price;
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
--- src/main/java/com/example/productcatalogservice_mar2025/services/ProductService.java
+++ src/main/java/com/example/productcatalogservice_mar2025/services/ProductService.java
@@ -1,23 +1,47 @@
 package com.example.productcatalogservice_mar2025.services;
 
+import com.example.productcatalogservice_mar2025.dtos.FakeStoreProductDto;
+import com.example.productcatalogservice_mar2025.models.Category;
 import com.example.productcatalogservice_mar2025.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 public class ProductService implements IProductService {
 
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
     public List<Product> getAllProducts() {
         return null;
     }
 
     public Product getProductById(Long id) {
-        return null;
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        FakeStoreProductDto fakeStoreProductDto=restTemplate.getForEntity("https://fakestoreapi.com/products/{product_id}",
+                FakeStoreProductDto.class,id).getBody();
+        return from(fakeStoreProductDto);
     }
 
     public Product addProduct(Product product) {
         return null;
+    }
+
+    private Product from(FakeStoreProductDto fakeStoreProductDto){
+        Product product = new Product();
+        product.setId(fakeStoreProductDto.getId());
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setDescription(fakeStoreProductDto.getDescription());
+        product.setAmount(fakeStoreProductDto.getPrice());
+        product.setImageUrl(fakeStoreProductDto.getImage());
+        Category category = new Category();
+        category.setName(fakeStoreProductDto.getCategory());
+        product.setCategory(category);
+        return product;
     }
 
 }
